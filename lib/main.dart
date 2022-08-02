3274899d1126948c20258e954e0cc3e2557e6db0
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-02 14:53:51 +0300

    Added native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pokemon/app.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initialization(null);
   runApp(const MyApp());
 }
 
 
+Future initialization(BuildContext? context) async{
+  await Future.delayed(const Duration(seconds: 3));
+}

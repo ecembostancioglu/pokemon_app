c404d78e0383548c0d57b218893879c439266307
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-03 00:47:03 +0300

    Added Pokemon class

---
+++ lib/domain/model/pokemon.dart
@@ -0,0 +1,50 @@
+class Pokemon {
+  int? count;
+  String? next;
+  Null? previous;
+  List<Results>? results;
+
+  Pokemon({this.count, this.next, this.previous, this.results});
+
+  Pokemon.fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    next = json['next'];
+    previous = json['previous'];
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['count'] = count;
+    data['next'] = next;
+    data['previous'] = previous;
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Results {
+  String? name;
+  String? url;
+
+  Results({this.name, this.url});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data =  <String, dynamic>{};
+    data['name'] = name;
+    data['url'] = url;
+    return data;
+  }
+}

6838c38924059efd5cbf7a417fffec3f6ea2f656
pradana4648 <wakwaujaim@gmail.com>
2022-08-20 11:07:19 +0700

    chore: removed initiliaze binding

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,11 @@ import 'package:pokemon/app.dart';
 import 'package:pokemon/presentation/details_bloc/details_bloc.dart';
 import 'package:pokemon/presentation/pokemon_bloc/pokemon_bloc.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initialization(null);
+void main() {
   runApp(
-      MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => PokemonBloc()),
-            BlocProvider(
-                create: (context) => DetailsBloc()),
-          ],
-          child: MyApp()));
-}
-
-
-Future initialization(BuildContext? context) async {
-  await Future.delayed(const Duration(seconds:2));
+    MultiBlocProvider(providers: [
+      BlocProvider(create: (context) => PokemonBloc()),
+      BlocProvider(create: (context) => DetailsBloc()),
+    ], child: const MyApp()),
+  );
 }

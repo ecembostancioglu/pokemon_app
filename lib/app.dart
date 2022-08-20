dd8db4746f7732837f87e781022119a75aa8e38e
pradana4648 <wakwaujaim@gmail.com>
2022-08-20 11:06:53 +0700

    styles: change code styling

--- lib/app.dart
+++ lib/app.dart
@@ -5,15 +5,14 @@ import 'package:pokemon/presentation/view/home_page.dart';
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize:const Size(360,690),
-        builder: (context,widget)
-        => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: HomePage(),
-        ));
+      designSize: const Size(360, 690),
+      builder: (context, widget) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
   }
 }
